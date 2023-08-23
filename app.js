d9bc6b3c9b2529c91d871917ccef59f0095ca944
Dom dK <dom@devildog.co.za>
2023-08-23 17:31:02 +0200

    commit

---
+++ app.js
@@ -0,0 +1,37 @@
+/* Tutorial on Web Development BootCamp - Colt Steele - https://www.udemy.com/course/the-web-developer-bootcamp*/
+
+
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("#getJokeButton");
+
+
+const addNewJoke = async () => {
+    resetJokes(); // Reset jokes before adding a new one
+
+    const jokeText = await getDadJoke();
+    const newJoke = document.createElement("p");
+    newJoke.append(jokeText);
+    jokes.append(newJoke);
+};
+
+const resetJokes = () => {
+    jokes.innerHTML = ""; // Clear all existing jokes
+};
+
+const getDadJoke = async () => {
+    try {
+        const config = { headers: { Accept: "application/json" } };
+        const res = await axios.get("https://icanhazdadjoke.com/", config);
+        return res.data.joke;
+    } catch (e) {
+        return "NO JOKES AVAILABLE! SORRY :(";
+    }
+};
+
+button.addEventListener("click", addNewJoke);
+
+const loadJokeOnPageLoad = async () => {
+    await addNewJoke();
+};
+
+window.addEventListener("load", loadJokeOnPageLoad);
